e7324e8ffffd2792173d1720e9b7d17929288c60
Lucas Wagner <you@example.com>
2021-04-12 22:59:51 +0200

    changed folder structure for heroku deploy

--- djangotestproject/settings.py
+++ djangotestproject/settings.py
@@ -41,10 +41,12 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'frontend.apps.FrontendConfig',
     'spotify.apps.SpotifyConfig',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'https://localhost:3000',
+]
